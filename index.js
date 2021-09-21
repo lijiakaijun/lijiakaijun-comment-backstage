4eee217ef9ed68c5d55dd6d1c36ad9826a4a246f
lijiakaijun <55282876+lijiajunljj@users.noreply.github.com>
2021-09-22 00:34:55 +0800

    更新违禁词

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Application = require('@waline/vercel');
 
 module.exports = Application({
+  forbiddenWords: ['网站每日ip 1千，', '台湾独立', '香港独立', '退党', '三退', '九评', 'falun', '法轮', '明慧', 'minghui', '新唐人', 'ntdtv', 'ndtv', '真善忍', '六四', '六 四'. '八九', '坦克人', '开户籍', '快递空包', '环酱'],
   async postSave(comment) {
     // do what ever you want after save comment
   },
